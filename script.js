709f8909a151a5cfaca666791ee4e22c598f5c90
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-18 20:37:26 +0100

    made 2 variables (database & newsFeed) with some arrays

---
+++ script.js
@@ -0,0 +1,17 @@
+var database = [
+    {
+        username: "Reuben",
+        password: "Westside12"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all that learning!"
+    },
+    {
+        username: "Sally",
+        timeline: "Javascript is so coool!"
+    }
+];
